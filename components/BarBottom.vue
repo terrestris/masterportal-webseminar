e83dce1437431d0dd3bc713a7b2c78f5ede090ed
blitza@terrrestris.de <blitza@terrestris.de>
2022-07-04 15:14:15 +0200

    update assets

---
+++ components/BarBottom.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+    },
+  }
+}
+</script>
+
+<template>
+  <div v-if="title || social" class="flex flex-row bg-barBottom absolute w-full bottom-0 left-0 text-lg">
+      <div class="basis-5" />
+      <div class="basis-8" />
+      <div class="basis-auto">4</div>
+    <!-- <div class="w-1/2 text-left">
+      {{ title }}
+    </div>
+    <div class="w-1/2 flex justify-end">
+      <slot />
+    </div> -->
+  </div>
+</template>
---
+++ layouts/start.vue
@@ -0,0 +1,28 @@
+
+<!-- default.vue -->
+<template>
+  <div class="slidev-layout default cover center">
+    <slot />
+    <div class="bottom-right-box" />
+  </div>
+</template>
+
+<style>
+.cover {
+    background-image: url("public/header_upper_left.svg");
+    background-repeat: no-repeat;
+    margin: auto;
+}
+.center {
+  display: flex;
+  justify-content: center;
+}
+.bottom-right-box {
+    width: 2rem;
+    height: 2rem;
+    background: #19204c;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+}
+</style>
